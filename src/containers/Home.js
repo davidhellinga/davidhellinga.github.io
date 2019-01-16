878faba3bdb748a8d2d0795bdf02892bdeb7658d
I392037 <d.hellinga@student.fontys.nl>
2019-01-16 14:36:47 +0100

    fixed unused var

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,23 +2,23 @@ import {Component} from "react";
 import LoaderButton from "./Signup";
 import React from "react";
 
-function sortByElement(element) {
-    let sortOrder = 1;
-
-    // Sort decending if property is negative
-    if (element[0] === "-") {
-        sortOrder = -1;
-        element = element.substr(1);
-    }
-
-    return function (a, b) {
-        if (sortOrder === -1) {
-            return b[element].localeCompare(a[element]);
-        } else {
-            return a[element].localeCompare(b[element]);
-        }
-    }
-}
+// function sortByElement(element) {
+//     let sortOrder = 1;
+//
+//     // Sort decending if property is negative
+//     if (element[0] === "-") {
+//         sortOrder = -1;
+//         element = element.substr(1);
+//     }
+//
+//     return function (a, b) {
+//         if (sortOrder === -1) {
+//             return b[element].localeCompare(a[element]);
+//         } else {
+//             return a[element].localeCompare(b[element]);
+//         }
+//     }
+// }
 
 export default class extends Component {
     state = {}
