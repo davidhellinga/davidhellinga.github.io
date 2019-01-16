15d39eb43a2382e30e8e2996fe3a79e7ae4fd367
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-16 12:11:29 +0000

    Deploy davidhellinga/fun4-chrono-alpha to github.com/davidhellinga/fun4-chrono-alpha.git:gh-pages

---
+++ precache-manifest.ed2cdaa43ddaa549ff05f57d14cfa24b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "24208e50114a2cabbc77",
+    "url": "/static/css/main.cda900fd.chunk.css"
+  },
+  {
+    "revision": "24208e50114a2cabbc77",
+    "url": "/static/js/main.24208e50.chunk.js"
+  },
+  {
+    "revision": "7408c9b0f2ae8dfa89d8",
+    "url": "/static/js/1.7408c9b0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bf0f03e5d1ac99e5790fab66f75e8f6f",
+    "url": "/index.html"
+  }
+];
